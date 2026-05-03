e67c10c066901a13187f68aeaa1ddccf1522ded8
wheatfox <granquistronan107@gmail.com>
2026-05-03 18:06:13 +0800

    gitignore: rbnx-build/, __pycache__, etc.

--- rbnx-build/ws/install/setup.bash
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-export PYTHONPATH="/home/wheatfox/Documents/Code/robonix_slam_ws/explore_rbnx:/home/wheatfox/Documents/Code/robonix_slam_ws/explore_rbnx/proto_gen:${PYTHONPATH:-}"
